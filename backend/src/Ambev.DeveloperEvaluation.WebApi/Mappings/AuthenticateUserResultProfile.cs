6ae483088a8abd94d766ba2765215f944cd20276
Gustavo Luiz Campolina Telles <gusstavotelles@outlook.com>
2025-09-01 21:10:04 -0300

    Fix: Update AuthenticateUserResult mapping to match API DTO

--- backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/AuthenticateUserResultProfile.cs
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/AuthenticateUserResultProfile.cs
@@ -13,10 +13,8 @@ public class AuthenticateUserResultProfile : Profile
     {
         CreateMap<AuthenticateUserResult, AuthenticateUserResponse>()
             .ForMember(dest => dest.Token, opt => opt.MapFrom(src => src.Token))
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-            .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
     }
 }
